9769a375473e51bc6b45a47d4c1123f7a3e0486a
luizlaljr <luizlaljr@gmail.com>
2022-01-23 11:25:52 -0300

    Removido a consulta na classe colecao e curso por imcompatibilidade.

--- src/main/java/br/uff/id/ProjetoDAC_Api/repository/ColecaoRepository.java
+++ src/main/java/br/uff/id/ProjetoDAC_Api/repository/ColecaoRepository.java
@@ -6,22 +6,13 @@
 package br.uff.id.ProjetoDAC_Api.repository;
 
 import br.uff.id.ProjetoDAC_Api.model.Colecao;
-import java.util.Calendar;
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  *
  * @author Luiz Alberto Junior
  */
 @RepositoryRestResource(collectionResourceRel = "colecao", path = "colecao")
-public interface ColecaoRepository extends CrudRepository<Colecao, Long>{
-    
-    @Query("SELECT c FROM Colecao c WHERE c.inicio >= :inicio AND c.fim <= :fim")
-    List<Colecao> findByEventosEntreDatas(@Param("inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Calendar inicio,@Param("fim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Calendar fim);
-    
+public interface ColecaoRepository extends CrudRepository<Colecao, Long>{     
 }
