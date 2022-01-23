38d361b4037214db6d1309b7e5e78fa5cb1a684f
luizlaljr <luizlaljr@gmail.com>
2022-01-23 08:59:12 -0300

    Configurado postgres do Heroku

---
+++ src/main/java/br/uff/id/ProjetoDAC_Api/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package br.uff.id.ProjetoDAC_Api;
+
+import javax.activation.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ *
+ * @author Luiz Alberto Junior
+ * @email luiz_alberto@id.uff.br
+ * Trabalho da disciplina Desenvolvimento de Aplicações Corporativas 2021/2
+ */
+@Configuration
+@PropertySource({ "classpath:application.properties" })
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return (DataSource) DataSourceBuilder.create().build();
+    }
+}
+
+
